13ffb28b5aad6c5cb864c2ab26b66617b614c0fe
Duvan Molina <duvanmoliina@gmail.com>
2021-02-12 23:41:31 -0500

    fix(get): get one verb

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,7 +1,16 @@
 import loadVerb from "../../mockup/data";
 
+interface Verb {
+  id: any;
+}
+
 export const resolvers = {
   Query: {
     verbs: () => loadVerb(),
+    verb: (_: any, arg: Verb) => {
+      const allVerb = loadVerb();
+      const verb = allVerb.filter((item) => item.id === parseInt(arg.id));
+      return verb[0];
+    },
   },
 };
--- src/graphql/typeDefs/index.ts
+++ src/graphql/typeDefs/index.ts
@@ -3,7 +3,9 @@ import { gql } from "apollo-server";
 export const typeDefs = gql`
   type Query {
     verbs: [ArrVerb]
+    verb(id: ID!): ArrVerb
   }
+
   type ArrVerb {
     id: ID
     name: String
